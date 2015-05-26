b500c5901df517a2b0d9f83b624cfe414910f00e
Nabil Hashmi <nabil2011@gmail.com>
2015-05-26 12:49:16 -0400

    Initial commit

---
+++ lib/overcast-time.rb
@@ -0,0 +1,65 @@
+require 'rubygems'
+require 'mechanize'
+require 'figaro'
+require 'nokogiri'
+require 'io/console'
+
+class OvercastTime
+
+  def initialize
+    puts "Please enter your Overcast.fm username (your email address)."
+    email = gets.chomp
+    print "Please enter your Overcast.fm password: "
+    password = STDIN.noecho(&:gets).chomp
+    fname = "lib/application.yml"
+    secrets = File.open(fname, "w")
+    secrets.puts("overcast_user_name: " + email)
+    secrets.puts("overcast_password: " + password)
+    secrets.close
+    Figaro.application = Figaro::Application.new(environment: "production", path: "application.yml")
+    Figaro.load
+
+    agent = Mechanize.new
+
+    page = agent.get("http://overcast.fm")
+
+    login_page = agent.page.link_with(:text => 'Log In').click
+
+    login_form = login_page.forms[0]
+
+    puts ENV['overcast_user_name']
+
+    login_form.email = ENV['overcast_user_name']
+    login_form.password = ENV['overcast_password']
+
+    podcasts = agent.submit(login_form, login_form.buttons[2])
+
+    times = podcasts.search('div.caption2')
+
+    total_seconds = 0
+
+    times.each do |time|
+      timestamp = time.text.match(/([0-9]+:)([0-9]+:)([0-9]+)/)
+      if timestamp
+        hours = timestamp[1][0...-1].to_i
+        total_seconds += hours * 3600
+        minutes = timestamp[2][0...-1].to_i
+        total_seconds += minutes * 60
+        seconds = timestamp[3].to_i
+        total_seconds += seconds
+      end
+    end
+
+    puts "Seconds: " + total_seconds.to_s
+
+    mm, ss = total_seconds.divmod(60)
+    hh, mm = mm.divmod(60)
+
+    puts "You have %d hours, %d minutes, and %d seconds of unheard podcasts remaining." % [hh, mm, ss]
+
+    File.delete("lib/application.yml")
+
+    return total_seconds
+
+  end
+end
---
+++ overcast-time.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.author = "Nabil Hashmi"
+  s.email = "nabil2011@gmail.com"
+  s.name = "overcast-time"
+  s.files = ["lib/overcast-time.rb", "lib/application.yml"]
+  s.version = "1.0.0"
+  s.homepage = "https://github.com/nhashmi/overcast-time"
+  s.executables << "overcast-time"
+  s.summary = "Calculate the time remaining on your Overcast.fm podcasts."
+end
